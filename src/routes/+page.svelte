9af633fab9fc5b1eccbf604c9dc92a4383b9e02a
DML <djamel.dyne.gaming@gmail.com>
2023-05-10 00:41:04 +0200

    code push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import './styles.css';
 	import img1 from '../lib/images/img1.png'
+	import trailer from '../lib/images/trailer.mp4'
 </script>
 
 <svelte:head>
@@ -21,6 +22,19 @@
 		</div>
 	</div>	  
 	<h1>Bienvenue sur Wizards Network</h1>
+	<div style="display:flex; flex-direction:row;">
+		<div style="flex:1;">
+		  <video src={trailer} width="640" height="540" controls></video>
+		</div>
+		<div class="presentation" style="flex:1;">
+			<p>Bonjour et bienvenue sur le serveur Minecraft Bedrock édition de Wizards Network, je suis Luna, et je suis ravie de te présenter notre univers de jeu passionnant !<br><br>
+				Wizards Network, comme son nom l'indique, est un lieu où la magie opère et où les joueurs peuvent s'affronter dans des modes de jeux passionnants. Nous avons actuellement deux modes de jeux : PvP Faction et Practice, où tu peux mettre tes compétences à l'épreuve en affrontant d'autres joueurs pour la suprématie de ta faction.<br><br>
+				Mais ce n'est pas tout, nous sommes également en train de préparer l'arrivée prochaine du mode de jeu SkyBlock, où tu pourras construire ta propre île dans le ciel et explorer de nouveaux horizons.<br><br>
+				Sur notre serveur, nous avons également une boutique où tu peux acheter des objets à un prix abordable pour t'aider à progresser plus rapidement et à te démarquer des autres joueurs. N'hésite pas à <a href="https://wizards-network-boutique.tebex.io/" target="_blank">y faire un tour</a> et à soutenir notre serveur en faisant des achats !<br><br>
+				Enfin, notre communauté est chaleureuse et accueillante, et nous avons des membres du personnel compétents pour t'aider en cas de besoin. Nous organisons également des événements réguliers pour que tu puisses t'amuser avec les autres joueurs.<br><br>
+				Alors, <a href="https://discord.gg/wizardsmc" target="_blank">rejoins-nous</a> dès maintenant sur Wizards Network et découvre un monde magique et passionnant qui t'attend !</p>
+		  </div>
+	  </div>	  
 </section>
 
 <style>
@@ -39,6 +53,7 @@
 	}
 
 	.img1 {
+		filter: blur(2px) brightness(75%);
 		width: 100vw;
 		height: 36vw;
 	}
@@ -56,7 +71,7 @@
 		margin-left: 40px;
 		margin-bottom: 35px;
 		padding: 10px;
-		background: linear-gradient(210deg, #7473e8aa, #26249caa);
+		background: linear-gradient(210deg, #5453b0aa, #161466aa);
         font-size: 14px;
         border-radius: 20px;
         color: rgb(255, 255, 255);
@@ -79,5 +94,14 @@
 	
 	h1 {
 		font-size: 30px;
+	}
+
+	.presentation {
+		margin-left: 10px;
+		margin-top: 5px;
+	}
+
+	p {
+		font-size: 12px;
 	}
 </style>
