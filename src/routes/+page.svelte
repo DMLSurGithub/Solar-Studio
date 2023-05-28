c2d41d792bf6880161a72996017e591d1a54bc22
DML <djamel.dyne.gaming@gmail.com>
2023-05-28 19:05:29 +0200

    code push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,13 @@
 <script>
 	import Header from '../lib/components/header/Header.svelte';
 	import Footer from '../lib/components/footer/Footer.svelte';
-	import Icone from '../lib/images/icon.png';
 </script>
 
 <svelte:head>
 	<title>Solar Studio - Accueil</title>
 	<meta name="description" content="Site officiel de Solar Studio, dessus vous pourrez retrouver des créations Web, Bot Discord et Add-Ons hors normes !" />
 	<meta content="#ffbc00" data-react-helmet="true" name="theme-color"/>
-	<meta property="og:image" content={Icone} />
+	<meta property="og:image" content="https://i.ibb.co/SP6L5ZQ/solar-studio.png" />
 </svelte:head>
 
 <Header />
