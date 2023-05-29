e5f2e13cec190631a65b54e08d77840166fe4bd6
DML <djamel.dyne.gaming@gmail.com>
2023-05-29 22:27:31 +0200

    code push

--- src/lib/components/presentation/Presentation.svelte
+++ src/lib/components/presentation/Presentation.svelte
@@ -1,9 +1,13 @@
 <script>
   import wallpaper from '../../images/wallpaper.jpg'
+  import logo from '../../images/logo.png'
 </script>
 
 <presentation class="presentation">
-  <img src={wallpaper} alt="Wallpaper" class="wallpaper"/>
+  <div class="wallpaper-container">
+    <img src={wallpaper} alt="Fond d'écran" class="wallpaper"/>
+    <img src={logo} alt="Logo" class="logo"/>
+  </div>
   <div class="text-zone">
     <p class="txt">Découvrez Solar Studio, le meilleur studio de la Galaxie pour tous vos besoins de développement ! Notre équipe d'experts vous propose une gamme de services spécialisés dans trois domaines inspirés par l'infini de l'espace.<br><br>Tout d'abord, en tant qu'adeptes du développement Web, nous maîtrisons le Framework Svelte, un vaisseau spatial technologique pour créer des sites Web innovants. Avec Svelte, nous concevons des interfaces interactives et rapides, transportant vos utilisateurs au-delà des étoiles.<br><br>Ensuite, notre expertise en développement de Bots Discord avec TypeScript donne vie à votre serveur. Nos Bots personnalisés, programmés pour automatiser des tâches spécifiques, offrent une expérience de chat intergalactique unique à vos utilisateurs.<br><br>Enfin, notre spécialisation dans les Add-Ons pour Minecraft Bedrock vous permet d'explorer des horizons infinis. Des packs de textures cosmiques aux fonctionnalités inédites, nous créons des extensions passionnantes pour enrichir votre expérience dans l'univers de Minecraft.<br><br>Chez Solar Studio, nous nous engageons à fournir des solutions de développement de qualité supérieure, rivalisant avec les étoiles les plus brillantes de la Galaxie. Notre équipe talentueuse et passionnée est déterminée à créer des expériences utilisateur uniques.</p>
   </div>
